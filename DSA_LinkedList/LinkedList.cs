de7d59823bcef109e874f1dededaf1cde5e10021
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-18 20:13:54 +0530

    Welcome To DataStructure

---
+++ DSA_LinkedList/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DSA_LinkedList
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        public void add(int id)
+        {
+            Node node = new Node(id);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted in to linked list", node.id);
+        }
+    }
+}
---
+++ DSA_LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DSA_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To DataStructure\n");
+            LinkedList list = new LinkedList();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+        }
+    }
+}
